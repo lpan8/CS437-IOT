a2c23e1d613d1f286ee6f593d6d024d8d0751999
ranganadh <ranganadh.karella@gmail.com>
2022-02-05 15:37:02 -0600

    Experiment only: routing along the path using a original advanced map

--- routing.py
+++ routing.py
@@ -2,10 +2,24 @@ from point import Point
 import heapq
 from typing import *
 import numpy as np
+import object_dist_mapping_working_bck as map
+import speed as sp
+import time
+import picar_4wd as fc
 
 T = TypeVar('T')
 
-temp_var = np.array([10])
+ANGLE_RANGE = 180
+
+#STEP indicates the angle steps
+STEP = 12
+us_step = STEP
+#curr angle -90 indicates right most position of car
+current_angle = -90
+max_angle = ANGLE_RANGE/2
+min_angle = -ANGLE_RANGE/2
+
+
 class PriorityQueue:
     def __init__(self):
         self.elements: List[Tuple[float, T]] = []
@@ -53,7 +67,6 @@ class Route:
 
             for next in Route.neighbors(map, current):
                 parent = came_from.get(current, None)
-
                 # Favor straight lines for calculating costs
                 new_cost = cost_so_far[current] + 1
 
@@ -72,27 +85,94 @@ class Route:
 
         return came_from, cost_so_far
 
+def route():
+    speed4 = sp.Speed(25)
+    speed4.start()
+    # time.sleep(2)
+    #fc.forward(30)
+    #fc.backward(30)
+    x = 0
+    for i in range(20):
+        time.sleep(0.1)
+        speed = speed4()
+        x += speed * 0.1
+        print("%smm/s"%speed,x)
+    print("%smm"%x)
+    speed4.deinit()
+    #fc.stop()
+
 
 if __name__ == '__main__':
-    map = np.array([[0, 1, 1, 1, 1, 1],
-                     [0, 0, 0, 0, 0, 0],
-                     [0, 1, 1, 1, 0, 0],
-                     [0, 1, 0, 1, 1, 0],
-                     [0, 1, 0, 1, 1, 0]])
 
+    #map = np.array([[0, 1, 1, 1, 1, 1],
+    #                 [0, 0, 0, 0, 0, 0],
+    #                 [0, 1, 1, 1, 0, 0],
+    #                 [0, 1, 0, 1, 1, 0],
+    #                 [0, 1, 0, 1, 1, 0]])
+
+    map = map.meas_dist_fill_dist_angle_bitmap(int(ANGLE_RANGE/STEP))
+    print(map)
     new_map = np.full(np.shape(map), -1)
 
     start = Point(0, 0)  # starting position
-    end = Point(5, 4)  # ending position
+    end = Point(35, 35)  # ending position
 
     came_from, cost_so_far = Route.search(map, start, end)
     for point, cost in cost_so_far.items():
         new_map[point.y][point.x] = cost
-
-    print(new_map)
+    path = np.zeros([70,2],dtype=int)
+    #print(new_map)
+    #print("before end",last_point)
     last_point = end
-    print(last_point)
-
+   i =69
     while last_point is not None:
         last_point = came_from[last_point]
-        print(last_point)
+        #print("in loop and i ",last_point,i)
+        if i != 0:
+            path[i,0]=last_point.x
+            path[i,1]=last_point.y
+            i-=1
+    print("path",path)
+    #
+    #route()
+    speed4 = sp.Speed(25)
+    speed4.start()
+
+    init_x = path[0,0]
+    init_y = path[0,1]
+    for x in range(36):
+        if x != 0:
+            if path[x,1] - path[x+1,1] == 0:
+                continue
+            else:
+                cur_x = path[x,0]-init_x
+                cur_y = path[x,1]-init_y
+                print("cur_x,cur_y",cur_x,cur_y)
+                fc.forward(30)
+                rel_dist = 0
+                while True:
+                    time.sleep(0.1)
+                    speed = speed4()
+                    rel_dist += speed * 0.1
+                    print("%smm/s"%speed,x)
+
+                    if ( rel_dist > (cur_x * 4) ):
+                        fc.turn_left(70)
+                        time.sleep(0.8)
+                        fc.stop()
+                        print("rel dis",rel_dist)
+                        break
+                init_x = cur_x
+                init_y = cur_y
+                rel_dist=0
+                while True:
+                    fc.forward(30)
+                    time.sleep(0.1)
+                    speed = speed4()
+                    rel_dist += speed * 0.1
+                    if ( rel_dist > 34 * 4 ):
+                        fc.stop()
+                        break
+            break
+    speed4.deinit()
+
