c0652cd8991ab09f8250f70e29d5904a5f400125
ranganadh <ranganadh.karella@gmail.com>
2022-02-06 19:48:00 -0600

    advanced map using cartisian coordinates for 180 degrees

--- object_dist_mapping.py
+++ object_dist_mapping.py
@@ -11,23 +11,39 @@ import sys
 
 
 ANGLE_RANGE = 180
-
-#STEP indicates the angle steps
 STEP = 12
 us_step = STEP
-#curr angle -90 indicates right most position of car
 current_angle = -90
 max_angle = ANGLE_RANGE/2
 min_angle = -ANGLE_RANGE/2
+speed = 20
 
-#DS to store the measured angle and distance points
+
+#The maximum number of coordinate points for map. Change it accordingly to the requirement
+MAP_ARRAY_MAX_X = 36
+MAP_ARRAY_MAX_Y = 36
+
+#Example: Car position is at (5,0) for (10,8) map as indicated below
+# 
+
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#Car Position->  0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+#                0 0 0 0 0 0 0 0
+
+
 angle_dist_list  = np.zeros([int(ANGLE_RANGE/STEP+1),2],dtype=int)
 
-MAX_DISTANCE = 100
-#map_array: 2D grid for distance and angle bitmap for one sweep for 180 degrees.
-#Row: indicates distance in cm
-#Column: indicates angle from 0 to 179
-map_array = np.zeros([MAX_DISTANCE,180],dtype=int)
+dist_between_points = np.zeros(int(ANGLE_RANGE/STEP),dtype=int)
+
+cart_map_temp = np.zeros_like(angle_dist_list)
+map_x_y = np.zeros([MAP_ARRAY_MAX_X,MAP_ARRAY_MAX_Y],dtype=int)
 
 servo = Servo(PWM("P0"), offset=0)
 
@@ -43,22 +59,19 @@ def get_distance_at(angle):
     angle_distance = [angle, distance]
     return distance
 
-
-#The below method measures distacne for various angles and fills up bitmap for distance and angle in 2D grid.
 def meas_dist_fill_dist_angle_bitmap(step_input):
     global angle_dist_list, current_angle, us_step
     status =0
-    map_array.fill(0)
+    dist_between_points.fill(0)
+    map_x_y.fill(0)
+    cart_map_temp.fill(0)
+
+
     for index in range(0,step_input+1) :
         dist = int(get_distance_at(current_angle))
         temp_angle = current_angle+90
         angle_dist_list[index,0]= temp_angle
         angle_dist_list[index,1]=dist
-        if ( index > 0 ) and ( dist < MAX_DISTANCE ) and ( angle_dist_list[index,1] > 0 ) and ( abs (angle_dist_list[index,1]-angle_dist_list[index-1,1]) <= 2.0 ):
-            if  angle_dist_list[index-1,0] < angle_dist_list[index,0] :
-                map_array[angle_dist_list[index,1],angle_dist_list[index-1,0]:angle_dist_list[index,0]]=1
-            else:
-                map_array[angle_dist_list[index,1],angle_dist_list[index,0]:angle_dist_list[index-1,0]]=1
 
         if ( index != step_input ):
             current_angle += us_step
@@ -68,18 +81,47 @@ def meas_dist_fill_dist_angle_bitmap(step_input):
         elif current_angle <= min_angle:
             current_angle = min_angle
             us_step = STEP
+    for k in range(angle_dist_list.shape[0]):
+        if (angle_dist_list[k,1] != -2 ):
+            cos_theta = np.cos(np.deg2rad(angle_dist_list[k,0]))
+            cart_map_temp[k,0] = angle_dist_list[k,1] * cos_theta
+
+            sin_theta = np.sin(np.deg2rad(angle_dist_list[k,0]))
+            cart_map_temp[k,1] = angle_dist_list[k,1] * sin_theta
+
+    for k in range(cart_map_temp.shape[0]):
+        if ( k>0 and cart_map_temp[k-1,0] !=0 and cart_map_temp[k-1,1] !=0 and cart_map_temp[k,0] !=0 and cart_map_temp[k,1] != 0):
+            dist_between_points[k-1] = np.sqrt( ((cart_map_temp[k-1,0]-cart_map_temp[k,0])**2) + ((cart_map_temp[k-1,1]-cart_map_temp[k,1])**2) )
+
+    for j in range(dist_between_points.shape[0]):
+        x = np.zeros(20,dtype=int)
+        y = np.zeros(20,dtype=int)
+        if dist_between_points[j] != 0 :
+            for i in range(int(dist_between_points[j])):
+                x = (int)( cart_map_temp[j:j+1,0] ) + ( (i/dist_between_points[j]) * (cart_map_temp[j+1:j+2,0] - cart_map_temp[j:j+1,0]))
+                y = (int)( cart_map_temp[j:j+1,1] ) + ( (i/dist_between_points[j]) * (cart_map_temp[j+1:j+2,1] - cart_map_temp[j:j+1,1]))
+                yy = y[:].astype(int)
+                if (x < MAP_ARRAY_MAX_X/2 and y < MAP_ARRAY_MAX_Y):
+                    x += MAP_ARRAY_MAX_X/2
+                    xx = x[:].astype(int)
+                    map_x_y[xx,yy]=1
+
 
     if current_angle == min_angle or current_angle == max_angle:
-        tmp = map_array.copy()
-        return tmp
+        if us_step < 0:
+            print("reverse")
+
+        final_map = map_x_y.copy()
+        return final_map
     else:
         return False
 
+
 def main():
     while True:
-        map_array_l = meas_dist_fill_dist_angle_bitmap(int(ANGLE_RANGE/STEP))
-        #print(map_array_l)
-
+        map = meas_dist_fill_dist_angle_bitmap(int(ANGLE_RANGE/STEP))
+        #print(map)
+        continue
 
 if __name__ == "__main__":
     try:
