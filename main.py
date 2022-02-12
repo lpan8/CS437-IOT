f896f4805ed0f224fe6afe0e0a913fc4228bbfbb
Lily Pan <55028787+lpan8@users.noreply.github.com>
2022-02-12 15:33:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -144,11 +144,13 @@ def dist_to_time(dist):
 def turn_90_right():
     fc.turn_right(100)
     time.sleep(0.8)
+    fc.stop()
 
 
 def turn_90_left():
     fc.turn_left(100)
     time.sleep(0.95)
+    fc.stop()
 
 def get_path(map, start, end):
     new_map = np.full(np.shape(map), -1)
